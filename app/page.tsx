7071e35b83d94703baa6b13819155d2080078762
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-12 19:20:05 +0200

    add: First commit, layout, assets and dependencies

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='flex-center h-screen'>
+      <h1 className='h1'>File Driver - The only Storage Solution you need</h1>
+    </div>
+  )
+}
