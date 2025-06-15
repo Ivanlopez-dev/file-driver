f7b422866493e61263fa94f9a5477deda44c1b6b
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-15 18:46:42 +0200

    feat: Add pages by file type

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -3,11 +3,14 @@ import { Models } from 'node-appwrite'
 import { getFiles } from '@/lib/actions/file.actions'
 import Sort from '@/components/Sort'
 import Card from '@/components/Card'
+import { getFileTypesParams } from '@/lib/utils'
 
 const Page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || ''
 
-  const files = await getFiles()
+  const types = getFileTypesParams(type) as FileType[]
+
+  const files = await getFiles({ types })
 
   return (
     <div className='page-container'>
