b35354de350bc0bed6a020c618403db05e5577fa
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-15 18:13:24 +0200

    feat: Delete file functionality

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -7,7 +7,11 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { actionsDropdownItems } from '@/constants'
 import { constructDownloadUrl } from '@/lib/utils'
-import { renameFile, updateFileUsers } from '@/lib/actions/file.actions'
+import {
+  deleteFile,
+  renameFile,
+  updateFileUsers
+} from '@/lib/actions/file.actions'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { FileDetails, ShareInput } from './ActionsModalContent'
@@ -56,7 +60,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
       rename: () =>
         renameFile({ fileId: file.$id, name, extension: file.extension, path }),
       share: () => updateFileUsers({ fileId: file.$id, emails, path }),
-      delete: () => console.log('delete')
+      delete: () =>
+        deleteFile({ fileId: file.$id, path, bucketFileId: file.bucketFileId })
     }
 
     success = await actions[action.value as keyof typeof actions]()
@@ -106,6 +111,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onInputChange={setEmails}
               onRemove={handleRemoveUser}
             />
+          )}
+          {value === 'delete' && (
+            <p className='delete-confirmation'>
+              Are you sure you want to delete{` `}
+              <span className='delete-file-name'>{file.name}</span>?
+            </p>
           )}
         </DialogHeader>
 
